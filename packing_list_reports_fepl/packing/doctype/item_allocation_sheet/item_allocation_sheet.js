5c6d9d1a6d67aad8a4c0361261e5912153f5d687
Regis Digital <regissdigital@gmail.com>
2026-05-16 08:10:11 +0530

    Implement Two-Step Allocation System (TL Quotas + SP Sub-Allocations)

--- packing_list_reports_fepl/packing/doctype/item_allocation_sheet/item_allocation_sheet.js
+++ packing_list_reports_fepl/packing/doctype/item_allocation_sheet/item_allocation_sheet.js
@@ -10,45 +10,6 @@ frappe.ui.form.on('Item Allocation Sheet', {
 	warehouse: function(frm) {
 		frm.trigger('update_stock');
 	},
-	refresh: function(frm) {
-		if (frm.doc.docstatus === 0 && frm.doc.item_code) {
-			frm.add_custom_button(__('Fetch Pending Orders'), function() {
-				frm.events.fetch_pending_orders(frm);
-			});
-		}
-	},
-	fetch_pending_orders: function(frm) {
-		if (!frm.doc.item_code) {
-			frappe.msgprint(__('Please select an Item Code first.'));
-			return;
-		}
-
-		frappe.call({
-			method: "get_pending_sales_orders",
-			doc: frm.doc,
-			args: {
-				item_code: frm.doc.item_code
-			},
-			callback: function(r) {
-				if (r.message && r.message.length > 0) {
-					frm.clear_table('allocations');
-					r.message.forEach(row => {
-						let child = frm.add_child('allocations');
-						child.sales_order = row.sales_order;
-						child.sales_order_item = row.sales_order_item;
-						child.customer = row.customer;
-						child.sales_partner = row.sales_partner;
-						// Optionally set allocated_qty to pending_qty or leave at 0
-						child.allocated_qty = 0; 
-					});
-					frm.refresh_field('allocations');
-					frappe.show_alert({message: __('Fetched {0} Pending Orders', [r.message.length]), color: 'blue'});
-				} else {
-					frappe.msgprint(__('No pending Sales Orders found for this item.'));
-				}
-			}
-		});
-	},
 	update_stock: function(frm) {
 		if (frm.doc.item_code && frm.doc.warehouse) {
 			frappe.call({
@@ -66,35 +27,6 @@ frappe.ui.form.on('Item Allocation Sheet', {
 					}
 				}
 			});
-		}
-	}
-});
-
-frappe.ui.form.on('Allocation Detail', {
-	sales_order: function(frm, cdt, cdn) {
-		let row = locals[cdt][cdn];
-		if (row.sales_order && frm.doc.item_code) {
-			// 1. Get the Sales Order Item ID
-			frappe.db.get_value('Sales Order Item', 
-				{ parent: row.sales_order, item_code: frm.doc.item_code }, 
-				'name', 
-				(r) => {
-					if (r && r.name) {
-						frappe.model.set_value(cdt, cdn, 'sales_order_item', r.name);
-						
-						// 2. Get Customer and Partner from Parent Sales Order
-						frappe.db.get_value('Sales Order', row.sales_order, ['customer_name', 'sales_partner'], (parent_res) => {
-							if (parent_res) {
-								frappe.model.set_value(cdt, cdn, 'customer', parent_res.customer_name);
-								frappe.model.set_value(cdt, cdn, 'sales_partner', parent_res.sales_partner);
-							}
-						});
-					} else {
-						frappe.msgprint(__('Item {0} not found in Sales Order {1}', [frm.doc.item_code, row.sales_order]));
-						frappe.model.set_value(cdt, cdn, 'sales_order', '');
-					}
-				}
-			);
 		}
 	}
 });
---
+++ packing_list_reports_fepl/packing/doctype/partner_sub_allocation/partner_sub_allocation.py
@@ -0,0 +1,45 @@
+import frappe
+from frappe.model.document import Document
+from frappe.utils import flt
+
+class PartnerSubAllocation(Document):
+	def validate(self):
+		self.validate_quota()
+
+	def on_submit(self):
+		self.create_stock_reservations()
+
+	def on_cancel(self):
+		self.cancel_stock_reservations()
+
+	def validate_quota(self):
+		total_allocated = sum(flt(row.allocated_qty) for row in self.sub_allocations)
+		if total_allocated > flt(self.quota):
+			frappe.throw(_("Total allocated quantity ({0}) exceeds your assigned quota ({1})").format(total_allocated, self.quota))
+
+	def create_stock_reservations(self):
+		for row in self.sub_allocations:
+			if row.allocated_qty > 0 and not row.stock_reservation_entry:
+				sre = frappe.get_doc({
+					"doctype": "Stock Reservation Entry",
+					"item_code": self.item_code,
+					"warehouse": self.warehouse,
+					"voucher_type": "Sales Order",
+					"voucher_no": row.sales_order,
+					"voucher_detail_no": row.sales_order_item,
+					"reserved_qty": row.allocated_qty,
+					"company": frappe.db.get_value("Sales Order", row.sales_order, "company") or frappe.defaults.get_global_default("company"),
+					"status": "Reserved"
+				})
+				sre.insert()
+				sre.submit()
+				row.db_set("stock_reservation_entry", sre.name)
+
+	def cancel_stock_reservations(self):
+		for row in self.sub_allocations:
+			if row.stock_reservation_entry:
+				sre_status = frappe.db.get_value("Stock Reservation Entry", row.stock_reservation_entry, "docstatus")
+				if sre_status == 1:
+					sre = frappe.get_doc("Stock Reservation Entry", row.stock_reservation_entry)
+					sre.cancel()
+				row.db_set("stock_reservation_entry", None)
