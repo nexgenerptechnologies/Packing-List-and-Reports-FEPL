e7be1a7bc1e747f685c6fe8b1896ef1043feaec9
Regis Digital <regissdigital@gmail.com>
2026-07-29 22:35:21 +0530

    feat: Add Supplier RFQ Export custom report

---
+++ packing_list_reports_fepl/packing/report/supplier_rfq_export/supplier_rfq_export.js
@@ -0,0 +1,11 @@
+﻿frappe.query_reports["Supplier RFQ Export"] = {
+	"filters": [
+		{
+			"fieldname": "quotation",
+			"label": __("Quotation Number"),
+			"fieldtype": "Link",
+			"options": "Quotation",
+			"reqd": 1
+		}
+	]
+};
